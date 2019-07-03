ba4ca53b114bdb8d667898ab1d66c909ed403231
Ketion <tani.topit@yandex.com>
2019-07-03 16:49:23 +0200

    added controllers and some routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes(/*[ 'register' => false ]*/);
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/dashboard', function (){
+    return view('dashboard.home');
+});
+
+Route::resource('perdorues', 'PerdoruesController');
+Route::resource('produkt', 'ProductController');
+Route::resource('porosi', 'PorosiController');
